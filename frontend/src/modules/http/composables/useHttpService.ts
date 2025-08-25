85c0f591d075c4f8a980aea472760f5e2238dddd
Артем Анпилов <d19amamac@d19amaMac.local>
2025-08-25 11:20:16 +0300

    feat: TRI-17 change fetch to useFetch

--- frontend/src/modules/http/composables/useHttpService.ts
+++ frontend/src/modules/http/composables/useHttpService.ts
@@ -1,65 +1,24 @@
+import {
+  type UseFetchOptions,
+  createFetch,
+  type useFetch,
+} from '@vueuse/core';
+
 interface UseHttpServiceReturn {
-  getData: <RETURN>(url: string) => Promise<RETURN | undefined>;
-  postData: <RETURN, BODY extends RETURN = RETURN>(url: string, body: BODY) => Promise<RETURN | undefined>;
-  patchData: <RETURN, BODY extends Partial<RETURN> = Partial<RETURN>>(url: string, body: BODY) => Promise<RETURN | undefined>;
-  deleteData: (url: string) => Promise<boolean>;
+  fetch: typeof useFetch;
 }
 
-export function useHttpService(): UseHttpServiceReturn {
-  const DEFAULT_HEADERS: HeadersInit = {
-    'Content-Type': 'application/json;charset=utf-8',
-  };
-
-  async function getData<RETURN>(url: string): Promise<RETURN | undefined> {
-    const response: Response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${url}`, {
-      method: 'GET',
-    });
-
-    return response.ok
-      ? await response.json()
-      : undefined;
-  }
-
-  async function postData<RETURN, BODY extends RETURN = RETURN>(url: string, body: BODY): Promise<RETURN | undefined> {
-    const response: Response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${url}`, {
-      method: 'POST',
-      headers: DEFAULT_HEADERS,
-      body: JSON.stringify({
-        ...body,
-      }),
-    });
-
-    return response.ok
-      ? await response.json()
-      : undefined;
-  }
-
-  async function patchData<RETURN, BODY extends Partial<RETURN> = Partial<RETURN>>(url: string, body: BODY): Promise<RETURN | undefined> {
-    const response: Response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${url}`, {
-      method: 'PATCH',
-      headers: DEFAULT_HEADERS,
-      body: JSON.stringify({
-        ...body,
-      }),
-    });
-
-    return response.ok
-      ? await response.json()
-      : undefined;
-  }
-
-  async function deleteData(url: string): Promise<boolean> {
-    const response: Response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${url}`, {
-      method: 'DELETE',
-    });
-
-    return response.ok;
-  }
+export function useHttpService(options?: UseFetchOptions): UseHttpServiceReturn {
+  const fetch: typeof useFetch = createFetch({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    combination: 'chain',
+    fetchOptions: {
+      mode: 'cors',
+    },
+    options,
+  });
 
   return {
-    getData,
-    postData,
-    patchData,
-    deleteData,
+    fetch,
   };
 }
