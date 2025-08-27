36136f5ca07177a0505db0e77359f05471842084
Артем Анпилов <d19amamac@d19amaMac.local>
2025-08-27 14:14:01 +0300

    feat: TRI-12 base geo api + key maps

---
+++ frontend/src/modules/geo/composables/useGeo.ts
@@ -0,0 +1,80 @@
+import {
+  type Ref,
+  ref,
+} from 'vue';
+import { useHttpService } from '@/modules/http';
+import type { GEO_ISO2 } from '@/modules/geo/types';
+
+interface UseGeoReturn {
+  isError: Ref<boolean>;
+  isLoading: Ref<boolean>;
+  getCountry: (iso2: GEO_ISO2) => Promise<void>;
+  getAllCountries: () => Promise<void>;
+}
+
+export function useGeo(): UseGeoReturn {
+  const {
+    fetch,
+  } = useHttpService({
+    baseUrl: import.meta.env.VITE_GEO_API_BASE_URL,
+    fetchOptions: {
+      headers: {
+        'X-CSCAPI-KEY': import.meta.env.VITE_GEO_API_KEY,
+      },
+    },
+  });
+
+  const isError = ref<boolean>(false);
+  const isLoading = ref<boolean>(false);
+
+  // TODO Убрать any!
+  const countries = ref<any[]>([]);
+  const country = ref<object>({});
+
+  async function getAllCountries(): Promise<void> {
+    isLoading.value = true;
+
+    const {
+      data,
+      error,
+    } = await fetch('/countries').get().json();
+
+    isLoading.value = false;
+
+    if (error.value) {
+      isError.value = true;
+      return;
+    }
+
+    if (data.value) {
+      countries.value = data.value;
+    }
+  }
+
+  async function getCountry(iso2: GEO_ISO2): Promise<void> {
+    isLoading.value = true;
+
+    const {
+      data,
+      error,
+    } = await fetch(`/countries/${iso2}`).get().json();
+
+    isLoading.value = false;
+
+    if (error.value) {
+      isError.value = true;
+      return;
+    }
+
+    if (data.value) {
+      country.value = data.value;
+    }
+  }
+
+  return {
+    isError,
+    isLoading,
+    getCountry,
+    getAllCountries,
+  };
+}
